d2c7ddb20b8b04eb0188328b0b644fbef6be0fe6
wzw-0708 <2589787957@qq.com>
2026-04-30 15:52:02 +0800

    Implement write_files function in file_utils.py
    Add a function to write multiple files to a specified directory.

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,1 +1,8 @@
+import os
 
+def write_files(files, base_dir="output"):
+    os.makedirs(base_dir, exist_ok=True)
+
+    for name, content in files.items():
+        with open(os.path.join(base_dir, name), "w") as f:
+            f.write(content)
